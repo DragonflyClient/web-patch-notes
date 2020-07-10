7459adb0f287f76ed78b6deb65c29df7089f8925
flaaaps <julianwening4@gmail.com>
2020-07-10 22:08:49 +0200

    Improved responsiveness of the news popup

--- main.js
+++ main.js
@@ -10,32 +10,58 @@ ham.addEventListener('click', toggleNav);
 
 // Open / Close the nav menu
 function toggleNav() {
-  nav.classList.toggle('nav-active');
-  ham.classList.toggle('ham-active');
-  socials.classList.toggle('socials-active');
+    if (nav.classList.contains("nav-active")) {
+        setTimeout(function () {
+            nav.style.display = "none"
+            console.log("here")
+        }, 400)
+    } else {
+        if(nav.classList.contains("dfo")) {
+            nav.classList.remove("dfo")
+        }
+        nav.style.display = "flex"
+    }
+    setTimeout(function () {
+        nav.classList.toggle('nav-active');
+        ham.classList.toggle('ham-active');
+        socials.classList.toggle('socials-active');
+    }, 5)
 }
 
 // Close the nav menu
 function closeMenu(e) {
-  nav.classList.remove('nav-active');
-  ham.classList.remove('ham-active');
+    nav.classList.remove('nav-active');
+    ham.classList.remove('ham-active');
 }
+
 function scrollToTop() {
-  $("html, body").animate({ scrollTop: 0 },  400)
-  closeMenu()
+    $("html, body").animate({scrollTop: 0}, 400)
+    closeMenu()
 }
+
+window.addEventListener("resize", function () {
+    // Nav
+    if (nav.classList.contains("nav-active")) {
+        setTimeout(function () {
+            nav.style.display = "none"
+            console.log("here")
+        }, 400)
+    } else {
+        nav.style.display = "flex"
+    }
+})
 
 // Change direction of logo anchor
 window.addEventListener("scroll", function () {
-  const logoImg = document.querySelector("#logo a")
-  if (document.documentElement.scrollTop > 0) {
-   /* logoImg.setAttribute("href", "''")*/
-    logoImg.removeAttribute("href")
-  } else {
-    logoImg.setAttribute("href", "https://inceptioncloud.net/dragonfly")
-  }
+    const logoImg = document.querySelector("#logo a")
+    if (document.documentElement.scrollTop > 0) {
+        /* logoImg.setAttribute("href", "''")*/
+        logoImg.removeAttribute("href")
+    } else {
+        logoImg.setAttribute("href", "https://inceptioncloud.net/dragonfly")
+    }
 })
 
 if ('scrollRestoration' in history) {
-  history.scrollRestoration = 'manual';
+    history.scrollRestoration = 'manual';
 }
