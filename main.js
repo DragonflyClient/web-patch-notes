9ab2a06012788fe0e6ae2c6262e880004f51ba2f
flaaaps <julianwening4@gmail.com>
2020-08-26 18:36:03 +0200

    🚀 Update scripts of header

--- main.js
+++ main.js
@@ -1,19 +1,85 @@
-function scrollToTop() { $('html, body').animate({ scrollTop: 0 }, 400), closeMenu(); }
-window.addEventListener('load', function () {
-    function closeMenu() { nav.classList.remove('nav-active'); ham.classList.remove('ham-active'); }
-    const nav = document.querySelector('.nav'), ham = document.querySelector('.ham-wrapper'), socials = document.querySelector('.socials'), header = document.getElementById('header') || document.getElementById('navbar'); ham.addEventListener('click', toggleNav); function toggleNav() {
-        nav.classList.toggle('nav-active')
-        ham.classList.toggle('ham-active'); setTimeout(() => { socials.classList.toggle("socials-active") }, 500);
-    }
-});
+// Hamburger Menu
+nav.classList.remove('nav-active');
+ham.classList.remove('ham-active');
+console.log('nav test active')
+let navOpen = false
+ham.addEventListener('click', e => {
 
-// Change logo href based on scroll position
-window.addEventListener('scroll', function () {
-    if (window.pageYOffset > 0) {
-        document.querySelector('.logo a').href = '#'
-        console.log(window.pageYOffset, 'greater')
+    if (!navOpen) {
+        nav.classList.add('nav-active')
+        ham.classList.add('ham-active')
+        socials.classList.add('socials-active')
+        if (news && news.getAttribute('closed') !== "true") {
+            console.log('first if')
+            setTimeout(() => {
+                hideNews()
+            }, 250);
+        } else {
+            console.log('first else')
+            hideNews()
+        }
+        navOpen = true
     } else {
-        document.querySelector('.logo a').href = 'https://playdragonfly.net'
-        console.log(window.pageYOffset, 'less')
+        if (news && news.getAttribute('closed') !== "true") {
+            showNews()
+            setTimeout(() => {
+                nav.classList.remove('nav-active')
+                ham.classList.remove('ham-active')
+                socials.classList.remove('socials-active')
+            }, 250);
+        } else {
+            nav.classList.remove('nav-active')
+            ham.classList.remove('ham-active')
+            socials.classList.remove('socials-active')
+        }
+
+        navOpen = false
     }
 })
+
+function hideNews() {
+    let op = "-"
+    if (news.getAttribute('closed') !== "true") {
+        news.style.transform = `translateY(${op}${news.offsetHeight}px)`
+        document.getElementById("navbar").style.transform = `translateY(${op}${news.offsetHeight}px)`
+        document.getElementById("navbar").style.top = `${news.offsetHeight}px`
+    }
+
+    console.log(op, `translateY(${op}${news.offsetHeight}px)`)
+
+    if (news.getAttribute('closed') == "true" && width < 1000) {
+        document.getElementById("features").style.paddingTop = "0px"
+        document.getElementById("features").style.marginTop = "-50px"
+    }
+    console.log(news.getAttribute('closed') == "true")
+    if (news.getAttribute('closed') == "true") {
+        landing.style.transform = `translateY(-${news.offsetHeight}px)`
+    }
+}
+function showNews() {
+    if (news.getAttribute('closed') !== "true") {
+        news.style.transform = `translateY(0px)`
+        document.getElementById("navbar").style.transform = `translateY(0px)`
+        document.getElementById("navbar").style.top = `0px`
+    }
+}
+
+if (news) {
+    newsCloseBtn.addEventListener("click", manageNews)
+}
+
+function manageNews() {
+    console.log('closing')
+    news.style.transform = `translateY(-${news.offsetHeight}px)`
+    document.getElementById("navbar").style.transform = `translateY(-${news.offsetHeight}px)`
+    document.getElementById("navbar").style.top = `${news.offsetHeight}px`
+    localStorage.setItem('newsClosed', 'true')
+    news.setAttribute('closed', 'true')
+    hideNews()
+}
+
+// Close the nav menu
+function closeMenu() {
+    nav.classList.remove('nav-active');
+    ham.classList.remove('ham-active');
+}
